16abce72ad8999782d57193d15da21e5b4a265d5
rxmn7 <jobirbek.r0058@gmail.com>
2025-08-18 13:17:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let ism = prompt("What's your name?");
+let surname = prompt("What's your surname?");
+let age = prompt("How old are you?");
+let city = prompt("Where do you live?");
+let country = prompt("What country are you from?");
+alert(`malumotlar to'g'ri kiritildimi`);
+
+console.log(`Name: ${ism}`);
+console.log(`Surname: ${surname}`);
+console.log(`Age: ${age}`);
+console.log(`City: ${city}`);
+console.log(`Country: ${country}`);
