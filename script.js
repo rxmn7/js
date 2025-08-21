03b06cd053cabfcf69f58dbdc6501c5a749f584e
rxmn7 <jobirbek.r0058@gmail.com>
2025-08-21 16:11:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let ism = prompt("What's your name?");
-let surname = prompt("What's your surname?");
-let age = prompt("How old are you?");
-let city = prompt("Where do you live?");
-let country = prompt("What country are you from?");
-alert(`malumotlar to'g'ri kiritildimi`);
+/*let a = 20;
+let b = 4;
+let P = b * a;
 
-console.log(`Name: ${ism}`);
-console.log(`Surname: ${surname}`);
-console.log(`Age: ${age}`);
-console.log(`City: ${city}`);
-console.log(`Country: ${country}`);
+console.log(P);
+
+let a1 = 12;
+let b1 = 2;
+let P1 = a1 / b1;
+
+console.log(P1);
+
+let a2 = 2;
+let b2 = 10;
+let c2 = 3;
+let P2 = a2 * b2 * c2; 
+
+console.log(P2);
+*/
+let s = prompt("String typedagi malumotlarni kiriting:");
+let n = prompt("Number typedagi sonini kiriting:");
+let b = prompt("Boolean typedagi qiymatini kiriting:");
+confirm("hamma malumotlar togriligiga ishonch hosil qildingizmi?")
+alert("Siz kiritgan malumotlar:\nString: " + s + "\nNumber: " + n + "\nBoolean: " + b);
